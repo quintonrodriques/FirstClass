46707cdb4e292a058191c1629dabba3d204d2935
Tristan Anderson <tristananderson6500@gmail.com>
2021-11-01 23:34:05 -0400

    Navigation and path traversal

--- FirstClass/Assets/Scripts/GM.cs
+++ FirstClass/Assets/Scripts/GM.cs
@@ -4,67 +4,89 @@ using UnityEngine;
 
 public class GM : MonoBehaviour
 {
+	public GameObject plane;
+	public GameObject waypoints;
+	public int planesPerMinute = 0;    
+	
+	private Vector3[] spawnPoints;                      //Holds spawn points for all spawn points
+	private Vector3[] spawnVectors = new Vector3[4];    //Holds spawn vector for all 4 walls
+	private bool gameRunning = true;
 
-    public GameObject plane;
+	private List<PlaneController> airplanePool;
 
-    public GameObject waypoints;
-    private Vector3[] spawnPoints = new Vector3[4];               //Holds spawn points for all 4 walls
-    private Vector3[] spawnVectors = new Vector3[4];               //Holds spawn vector for all 4 walls
+	void Start()
+	{
+		airplanePool = new List<PlaneController>();
 
-    public int planesPerMinute = 0;    
-    float spawnDelay = 0f;                //Delay time in-between each plane, in seconds
+		setSpawnWalls();
 
-    float elapsedTime = 0.0f;
+		float spawnDelay = 60f / planesPerMinute;
+		StartCoroutine(SpawnAirplane(spawnDelay));    
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        Vector3[] spawnPoints = new Vector3[4];
-        Vector3[] spawnVectors = new Vector3[4];
-        setSpawnWalls();
+		Debug.Log("Time in seconds between every plane: " + spawnDelay);
+	}
 
-        spawnDelay = 60f / (float) planesPerMinute;
-        Debug.Log("Time in seconds between every plane: " + spawnDelay);
-    }
+	void setSpawnWalls()
+	{
+		spawnPoints = new Vector3[waypoints.transform.childCount];
+		for (int i = 0; i < spawnPoints.Length; i++)
+			spawnPoints[i] = waypoints.transform.GetChild(i).position;
 
-    void setSpawnWalls()
-    {
-        spawnPoints[0] = waypoints.transform.GetChild(0).transform.position;
-        spawnPoints[1] = waypoints.transform.GetChild(1).transform.position;
-        spawnPoints[2] = waypoints.transform.GetChild(2).transform.position;
-        spawnPoints[3] = waypoints.transform.GetChild(3).transform.position;
+		// This assumes the first four spawn points are points along the wall;
+		for (int i = 0; i < 4; i++)
+			spawnVectors[i] = (spawnPoints[(i + 1) % 4] - spawnPoints[i]);
+	}
 
+	void spawn()
+	{
+		int selection = Random.Range(0, spawnPoints.Length);
+		Vector3 spawnPosition = spawnPoints[selection];
 
+		if (selection < 4) 
+		{
+			float positionAlongSpawnVector = Random.Range(0f, 1f);
+			spawnPosition += (positionAlongSpawnVector * spawnVectors[selection]);
+		}
 
-        spawnVectors[0] = spawnPoints[1] - spawnPoints[0];
-        spawnVectors[1] = spawnPoints[3] - spawnPoints[1];
-        spawnVectors[2] = spawnPoints[3] - spawnPoints[2];
-        spawnVectors[3] = spawnPoints[0] - spawnPoints[2];
-    }
+		int index = GetAvailableAirplaneIndex();
+		if (index < 0)
+		{
+			airplanePool.Add(Instantiate(plane, Vector3.zero, Quaternion.identity).GetComponent<PlaneController>());
+			index = airplanePool.Count - 1;
+		}
+		
+		airplanePool[index].transform.position = spawnPosition;
+		airplanePool[index].SetTarget(GetTargetAirport(spawnPosition));
+		airplanePool[index].BeginJourney();
+	}
 
-    void spawn()
-    {
-        int selection = Random.Range(0, 4);
-        float positionAlongSpawnVector = Random.Range(0f, 1f);
+	Vector3 GetTargetAirport(Vector3 spawnPosition)
+	{
+		Vector3 targetPoint;
+		do
+			targetPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+		while (targetPoint.x == spawnPosition.x || targetPoint.z == spawnPosition.z);
 
-        Vector3 spawnVector = spawnPoints[selection] + (positionAlongSpawnVector * spawnVectors[selection]);
+		return targetPoint;
+	}
 
-        Instantiate(plane, spawnVector, Quaternion.identity);
-    }
+	int GetAvailableAirplaneIndex()
+	{
+		for (int i = 0; i < airplanePool.Count; i++)
+		{
+			if (airplanePool[i].isAvailable)
+				return i;
+		}
 
-    // Update is called once per frame
-    void Update()
-    {
-        elapsedTime += Time.deltaTime;
-        if (elapsedTime >= spawnDelay)
-        {
-            elapsedTime = elapsedTime % 1f;
-            SpawnAirplane();
-        }
-    }
-    void SpawnAirplane()
-    {
-        Debug.Log("Plane spawned!" + elapsedTime);
-        spawn();
-    }
+		return -1;
+	}
+
+	IEnumerator SpawnAirplane(float timeBetweenSpawns)
+	{
+		while (gameRunning)
+		{
+			spawn();
+			yield return new WaitForSeconds(timeBetweenSpawns);
+		}
+	}
 }
--- FirstClass/Assets/Scripts/PlaneController.cs
+++ FirstClass/Assets/Scripts/PlaneController.cs
@@ -1,22 +1,90 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlaneController : MonoBehaviour
 {
-    Vector3 position;
-    Vector3 targetPosition;
+    public bool isAvailable = false;
+    public float flySpeed = 6.0f;
 
-    // Start is called before the first frame update
-    void Start()
+    [Range(0.0f, 1.0f)]
+    public float careFactor = 0.5f;
+    public int pathRequestsPerSecond = 1;
+    public float proximityThreshold = 0.5f;
+
+    public int travelIndex;
+    private Vector3 targetPosition;
+    private Vector3[] calculatedPath;
+
+    public void SetTarget(Vector3 target) { targetPosition = target; }
+
+    public void BeginJourney()
     {
+        gameObject.SetActive(true);
+
+        travelIndex = 1;
+        transform.LookAt(targetPosition);
+
+        calculatedPath = Pathfinding.FindPath(transform.position, targetPosition);
+
+        isRequestingPath = StartCoroutine(RequestPath(60.0f / pathRequestsPerSecond));
         
+        isAvailable = false;
     }
 
-    // Update is called once per frame
+    public void EndJourney()
+    {
+        if (isRequestingPath != null)
+        {
+            StopCoroutine(isRequestingPath);
+            isRequestingPath = null;
+        }
+
+        isAvailable = true;
+        gameObject.SetActive(false);
+    }
+
+    Coroutine isRequestingPath = null;
+    IEnumerator RequestPath(float timeBetweenRequests)
+    {
+        while (!isAvailable)
+        {
+            calculatedPath = Pathfinding.FindPath(transform.position, targetPosition);
+            yield return new WaitForSeconds(timeBetweenRequests);
+        }
+    }
+
+    void Start()
+    {
+
+    }
+
     void Update()
     {
-        transform.LookAt(new Vector3(0,0,0));
-        transform.eulerAngles = new Vector3(90f, transform.eulerAngles.y, transform.eulerAngles.z);
+        if (!isAvailable)
+            Travel();
+    }
+
+    void Travel()
+    {
+        Vector3[] path = new Vector3[calculatedPath.Length];
+        for (int i = 0; i < path.Length; i++)
+        {
+            Vector3 directPathPos = Vector3.Lerp(calculatedPath[0], targetPosition, (float)i / (path.Length - 1));
+            path[i] = Vector3.Lerp(directPathPos, calculatedPath[i], careFactor);
+        }
+
+        transform.LookAt(path[travelIndex]);
+        transform.position = Vector3.MoveTowards(transform.position, path[travelIndex], flySpeed * Time.deltaTime);
+        
+        if (Vector3.Distance(transform.position, path[travelIndex]) <= proximityThreshold)
+        {
+            if (travelIndex >= path.Length - 1)
+            {
+                EndJourney();
+                return;
+            }
+
+            travelIndex++;
+        }
     }
 }
